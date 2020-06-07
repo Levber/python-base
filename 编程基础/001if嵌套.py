18f361771f7a3e53437e300bdcc0b0239d3f3a84
Levber <1051225870@qq.com>
2020-06-07 13:51:39 +0800

    列表及循环

---
+++ 编程基础/001if嵌套.py
@@ -0,0 +1,20 @@
+'''
+猜拳游戏案例
+1.出拳
+    电脑：固定 1，剪刀；随机2
+2.判断输赢
+    1.玩家获胜
+    2.平局
+    3.电脑获胜
+'''
+#电脑随机出拳
+import random
+player = int(input("请输入数字代表出拳：0-石头，1-剪刀，2-布"))
+computer = random.randint(0,2)
+print(computer)
+if ((player==0)and(computer==1))or((player==1)and(computer==2))or((player==2)and(computer==0)):
+    print("玩家获胜！！")
+elif player == computer :
+    print("平局！！")
+else:
+    print("电脑获胜！！！")
---
+++ 编程基础/005列表.py
@@ -0,0 +1,11 @@
+'''
+列表：[1,2,3,4,5]
+'''
+name_list = ["Tom",'Lily','Rose']
+print(name_list[1])
+#1. index()
+print(name_list.index('Tom'))
+#2 count() 统计次数
+print(name_list.count('Tom'))
+#len()  统计长度
+print(len(name_list))
