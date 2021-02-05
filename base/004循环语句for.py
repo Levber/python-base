ee0fc0f39cd142db362822094fb30b99f498feaa
Levber <1051225870@qq.com>
2021-02-05 17:34:50 +0800

    基础知识

--- base/004循环语句for.py
+++ base/004循环语句for.py
@@ -8,4 +8,19 @@ for student in classmates:
 sum = 0
 for x in range(0, 100):
     sum = sum + x
-print(sum)
+print(sum)  # 在循环外打印就是最终的结果，如果在循环类打印就是每一次相加的结果
+
+n = 1
+while n < 100:
+    if n > 10:  # 打印出10以内的每一个正整数
+        break  # break跳出当前循环，结束
+    print(n)
+    n = n + 1
+print('END')
+
+x = 0
+while x < 10:
+    x = x + 1
+    if x % 2 == 0:  # 打印10以内取余为0 的正整数
+        continue  # continue 跳出循环继续执行
+    print(x)
